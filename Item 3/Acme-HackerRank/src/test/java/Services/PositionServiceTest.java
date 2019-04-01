29bef6bdad38a879c4ef637d8f15a43ad8ed9ffc
Juan Morón Vilán <jmoronvilan@gmail.com>
2019-04-01 20:06:46 +0200

    Position Junit

---
+++ Item 3/Acme-HackerRank/src/test/java/Services/PositionServiceTest.java
@@ -0,0 +1,24 @@
+
+package Services;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import utilities.AbstractTest;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {
+	"classpath:spring/junit.xml"
+})
+@Transactional
+public class PositionServiceTest extends AbstractTest {
+
+	@Test
+	public void createAndSaveDriver() {
+
+	}
+
+}
