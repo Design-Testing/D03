e718b487118214a17a25abdd61b297d53b99c1ec
Juan Morón Vilán <jmoronvilan@gmail.com>
2019-04-01 19:01:51 +0200

    Update

--- Item 3/Acme-HackerRank/src/main/java/repositories/PositionRepository.java
+++ Item 3/Acme-HackerRank/src/main/java/repositories/PositionRepository.java
@@ -24,6 +24,7 @@ public interface PositionRepository extends JpaRepository<Position, Integer> {
 	@Query("select p from Position p where p.mode='FINAL' AND p.company.id=?1")
 	Collection<Position> findAllFinalModeByCompany(int id);
 
+	//TODO:Hay que corregirla
 	@Query("select distinct p from Position p where p.mode='FINAL' AND (?1='' OR p.title LIKE CONCAT('%',?1,'%') OR p.description LIKE CONCAT('%',?1,'%') " + "OR p.profile LIKE CONCAT('%',?1,'%') OR p join p.skills s where s LIKE CONCAT('%',?1,'%') "
 		+ "OR p join p.technologies t where t LIKE CONCAT('%',?1,'%') OR p join p.company c where c.commercialName LIKE CONCAT('%',?1,'%'))")
 	Collection<Position> findPositions(String key);
